c6db12a5e69b8b1d16f8664960ca80b3c8ec971d
MarcosFernando <marcosfernando@id.uff.br>
2023-04-25 09:46:25 -0300

    form teste criado

--- lib/app/ui/new_movie/new_movie.dart
+++ lib/app/ui/new_movie/new_movie.dart
@@ -4,7 +4,8 @@ import 'package:get/get.dart';
 import 'package:summer_class_app/app/controllers/new_movie_controller.dart';
 
 class NewMoviePage extends GetView<NewMovieController> {
-  const NewMoviePage({Key? key}) : super(key: key);
+  NewMoviePage({Key? key}) : super(key: key);
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +13,49 @@ class NewMoviePage extends GetView<NewMovieController> {
       appBar: AppBar(title: const Text('New Movie Page')),
       body: GetBuilder<NewMovieController>(
         builder: (_) {
-          return const Center(child: Text("Work in progress"));
+          return Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Teste...',
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Teste2...',
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Validate will return true if the form is valid, or false if
+                      // the form is invalid.
+                      if (_formKey.currentState!.validate()) {
+                        // Process data.
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ),
+              ],
+            )
+          );
         }
       ),
     );
