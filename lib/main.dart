9774822a03178a86ff5f55b0e439c820f40198a7
MarcosFernando <marcosfernando@id.uff.br>
2023-12-27 16:55:16 -0300

    video1

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:summerclass/firebase_options.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
