671f038cd9fb9f49bb3dfba5da0c8262faa826cd
MarcosFernando <marcosfernando@id.uff.br>
2023-12-29 20:06:10 -0300

    video 5

--- lib/new_movie_page.dart
+++ lib/new_movie_page.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 
 class NewMoviePage extends StatefulWidget {
   const NewMoviePage({super.key});
@@ -11,10 +17,20 @@ class NewMoviePage extends StatefulWidget {
 class _NewMoviePageState extends State<NewMoviePage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final db = FirebaseFirestore.instance;
+  final storage = FirebaseStorage.instance;
 
   String? titulo;
   String? diretor;
   String? sinopse;
+  File? imagePath;
+  Uint8List? imageBytes;
+
+  @override
+  initState() {
+    super.initState();
+    loadAssetImage();
+    debugPrint("Details initState Called: $imagePath");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,6 +79,37 @@ class _NewMoviePageState extends State<NewMoviePage> {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: InkWell (
+                onTap: imagePicker,
+                child: imageBytes != null
+                   ? Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      image: DecorationImage(
+                        image: MemoryImage(imageBytes!),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    width: 100,
+                    height: 100,
+                  )
+                  : Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: const Icon(
+                      Icons.camera_alt,
+                      color: Colors.grey,
+                      size: 80,
+                    ),
+                  ),
+              )
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: ElevatedButton(
                 onPressed: () async {
                   // Validate retorna true se o form for válido
@@ -71,7 +118,7 @@ class _NewMoviePageState extends State<NewMoviePage> {
                     ScaffoldMessenger.of(context).showSnackBar(
                         const SnackBar(content: Text('Salvando...')));
                     _formKey.currentState!.save();
-                    await postMovie(titulo!, diretor!, sinopse!);
+                    await postMovie(titulo!, diretor!, sinopse!, imagePath!);
                     Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
                   }
                 },
@@ -85,8 +132,33 @@ class _NewMoviePageState extends State<NewMoviePage> {
     );
   }
 
-  postMovie(String titulo, String diretor, String sinopse) async {
+  Future<void> loadAssetImage() async {
+    final tempDir = await getTemporaryDirectory();
+    final tempPath = '${tempDir.path}/claquete.png';
+    ByteData assetData = await rootBundle.load('assets/claquete.png');
+    File(tempPath).writeAsBytesSync(assetData.buffer.asUint8List());
+    // File tempFile = File(tempPath);
+    imagePath = File(tempPath);
+    debugPrint(imagePath!.path);
+  }
+
+  imagePicker() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      Uint8List imageBytes = await pickedFile.readAsBytes();
+      setState(() {
+        this.imageBytes = imageBytes;
+      });
+      imagePath = File(pickedFile.path);
+      debugPrint("Image path: $imagePath");
+    }
+  }
+
+  postMovie(String titulo, String diretor, String sinopse, File imagePath) async {
     String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+    final ref = storage.ref().child(fileName); // criando referência
+    await ref.putFile(imagePath); // salvando imagem
 
     Map<String, dynamic> movie = {
       "titulo": titulo,
