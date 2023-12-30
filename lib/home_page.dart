a025b0d7873b2fb3aa8433ad77206dbb0c923d76
MarcosFernando <marcosfernando@id.uff.br>
2023-12-29 21:54:06 -0300

    video 6

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:vertical_card_pager/vertical_card_pager.dart';
 
@@ -9,20 +11,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<String> titles = ["BLUE", "GREY", ""];
+  final db = FirebaseFirestore.instance;
+  final storage = FirebaseStorage.instance;
 
-  final List<Widget> images = [
-    Container(
-      color: Colors.blue,
-    ),
-    Container(
-      color: Colors.grey,
-    ),
-    ClipRRect(
-        borderRadius: BorderRadius.circular(20.0),
-        child: const Icon(Icons.add_box_rounded, color: Colors.blue, size: 200)
-    ),
-  ];
+  bool isLoading = true;
+  late bool isLogged;
+  List<Map<String, dynamic>> moviesList = [];
+  List<String> titles = [];
+  List<Widget> images = [];
+
+  @override
+  initState() {
+    super.initState();
+    reloadData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,38 +33,98 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Filmes nacionais"),
         centerTitle: true,
-        actions: const [
+        actions: [
           IconButton(
-            onPressed: null,
-            icon: Icon(Icons.refresh)
-          )
+            onPressed: reloadData,
+            icon: const Icon(Icons.refresh)
+          ),
         ],
       ),
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: VerticalCardPager(
-                  titles: titles,  // required
-                  images: images,  // required
-                  textStyle: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold), // optional
-                  onSelectedItem: onSelectedItem,
-                  initialPage: 1, // optional
-              ),
-            ),
-          ],
+      body: isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : VerticalCardPager(
+          initialPage: 1,
+          titles: titles,
+          images: images,
+          onSelectedItem: onSelectedItem,
         ),
+    );
+  }
+
+  Future<void>getAll() async {
+    try{
+      List<Map<String, dynamic>> moviesList = [];
+      await db.collection("movies").get().then((querySnapshot) async {
+        final docs = querySnapshot.docs;
+        for (var index = 0; index < docs.length; index++) {
+          final docSnapshot = docs[index];
+
+          Map<String, dynamic> movie = {};
+
+          movie["id"] = docSnapshot.id;
+          movie["titulo"] = docSnapshot.data()["titulo"];
+          movie["diretor"] = docSnapshot.data()["diretor"];
+          movie["sinopse"] = docSnapshot.data()["sinopse"];
+          movie["liked"] = docSnapshot.data()["liked"];
+
+          movie["image"] = await storage.ref().child(docSnapshot.data()["image"]).getData();
+
+          moviesList.add(movie);
+        }
+      }, onError: (e) {
+        debugPrint("Error completing: $e");
+      });
+      // debugPrint("MovieList: $moviesList");
+      this.moviesList = moviesList;
+    } catch(e) {
+      debugPrint('ERRO db.collection("movies").get(): $e');
+    }
+  }
+
+  Future<void> fillMovieInfo(List movieList) async {
+    for(var movie in movieList){
+      titles.add("");
+      images.add(
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Image.memory(
+            movie["image"],
+            fit: BoxFit.cover,
+          ),
+        )
+      );
+    }
+
+    titles.add("");
+    images.add(
+      ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: const Icon(Icons.add_box_rounded, color: Colors.blue, size: 200)
       ),
     );
   }
 
+  Future<void> reloadData() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    titles = [];
+    images = [];
+    await getAll();
+    fillMovieInfo(moviesList);
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   void onSelectedItem(int index) {
-    debugPrint(titles.length.toString());
     debugPrint(index.toString());
     if (index == titles.length -1) {
       Navigator.pushNamed(context, "/new");
     } else {
-      debugPrint(titles[index]);
+      Navigator.pushNamed(context, '/details', arguments: moviesList[index]);
     }
   }
 }
